86e96e5d241a6e974d48b08f790b202d3eef904f
daniel <neskdan812@gmail.com>
2023-09-15 22:52:46 +0300

    initial commit

--- hang-snake/hangman/runTwo.py
+++ hang-snake/hangman/runTwo.py
@@ -3,39 +3,30 @@ from random import randint
 import dict
 import hangmanConfig
 
-
-def createSecret():
-    return str(dict.words[randint(0,len(dict.words)-1)])
-
-secretWord = createSecret()
+word = dict.words[randint(0,len(dict.words)-1)]
 tries = 6
 incorrectLetters = []
 correctLetters = []
-wordInProgress = ['_']*len(secretWord)
+wordInProgress = ['_']*len(word)
 hangman = hangmanConfig.guys[6-tries]
 
-def play():
-    print(secretWord)
+
+while tries > 0:
+    print(word)
     print(*wordInProgress)
-    global tries
-    global hangman
     letter = input('Enter your guess: ')
-    if len(letter)>1 or type(letter) != 'str':
-        return 'Write just one letter please'
+    if len(letter) > 1 or type(letter) != 'str':
+        print('Write just one letter please')
     else:
-        if secretWord.find(letter) == -1:
+        if word.find(letter) == -1:
             tries -= 1
             incorrectLetters.append(letter)
             hangman = hangmanConfig.guys[6 - tries]
             print(hangman)
         else:
-            for i in len(secretWord):
-                if letter == secretWord[i]:
+            tries = tries
+            for i in len(word):
+                if letter == word[i]:
                     wordInProgress[i] = letter
-        return
-
-while tries > 0:
-    play()
 
     # clear_terminal()
-    # print(FIELD)
